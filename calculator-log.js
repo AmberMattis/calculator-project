f63a5d14943fd447a11a8b2b97e79676d21835b2
Amber Mattis <amber.mattis2@gmail.com>
2022-11-08 21:19:03 -0600

    Implemented logic to append numbers to the display when buttons are clicked

---
+++ calculator-log.js
@@ -0,0 +1,58 @@
+
+
+
+//functions to append numbers to the calculator display
+
+function num1() {
+    document.getElementById("view").innerHTML = "1";
+}
+function num2() {
+    document.getElementById("view").innerHTML = "2";
+}
+function num3() {
+    document.getElementById("view").innerHTML = "3";
+}
+function num4() {
+    document.getElementById("view").innerHTML = "4";
+}
+function num5() {
+    document.getElementById("view").innerHTML = "5";
+}
+function num6() {
+    document.getElementById("view").innerHTML = "6";
+}
+function num7() {
+    document.getElementById("view").innerHTML = "7";
+}
+function num8() {
+    document.getElementById("view").innerHTML = "8";
+}
+function num9() {
+    document.getElementById("view").innerHTML = "9";
+}
+function num0() {
+    document.getElementById("view").innerHTML = "0";
+}
+function decimal() {
+    document.getElementById("view").innerHTML = ".";
+}
+
+
+//functions to append the actions to the calculator display
+
+function numAdd() {
+    document.getElementById("view").innerHTML = "+";
+}
+function numSubtract() {
+    document.getElementById("view").innerHTML = "-";
+}
+function numMultiply() {
+    document.getElementById("view").innerHTML = "*";
+}
+function numDivide() {
+    document.getElementById("view").innerHTML = "/";
+}
+function removeNums() {
+    document.getElementById("view").innerHTML = "";
+}
+
